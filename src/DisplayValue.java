8c1117b7dbf02eab651eda70536efd72eb1e5e06
succhi111 <suchendramishra396@gmail.com>
2022-08-25 10:51:32 +0530

    program to display the default value of primitive data types

---
+++ src/DisplayValue.java
@@ -0,0 +1,18 @@
+public class DisplayValue {
+    static boolean bool;
+    static double doub;
+    static float flo;
+    static int in;
+    static long lo;
+    static String str;
+
+    public static void main(String[] args) {
+        System.out.println("Default values of given premetive datatypes are:");
+        System.out.println("boolean = " + bool);
+        System.out.println("double = " + doub);
+        System.out.println("float = " + flo);
+        System.out.println("int = " + in);
+        System.out.println("long = " + lo);
+        System.out.println("string = " + str);
+    }
+}
