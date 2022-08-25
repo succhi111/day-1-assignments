40bcc491af15db5f475b589237518086adbd78b8
succhi111 <suchendramishra396@gmail.com>
2022-08-25 11:07:14 +0530

    program to compare two strings

---
+++ src/EqualString.java
@@ -0,0 +1,12 @@
+public class EqualString {
+    public static void main(String[] args) {
+        String s1="hello123";
+        String s2="HELLO123";
+
+        if (s1==s2)
+            System.out.println("Same String");
+            else
+            System.out.println("Different String");
+
+    }
+}
